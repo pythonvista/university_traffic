7bc4f7a1adb083b4bd60e568a9d42212630f50f8
pythonvista <nablintech@gmail.com>
2025-07-01 14:24:26 +0100

    a

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def inverse_transform(predictions, scaler):
     return predicted_temp
 
 
-    st.title("University Traffic Forecasting with LSTM")
+    st.title("University Network Prediction Using RAE (Recurrent Autoencoder)")
     
     # Load model and scaler
     model, scaler = load_model_and_scaler()
@@ -236,7 +236,7 @@ def Forecast(scaler, model):
 
 # Main Streamlit app
 def main():
-    st.title("University Traffic Forecasting with LSTM")
+    st.title("University Network Prediction Using RAE (Recurrent Autoencoder)")
     
     # Load model and scaler
     model, scaler = load_model_and_scaler()
